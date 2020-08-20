73472b288080ca9b194ded5babce1ade6027a741
student <student@DESKTOP-L3ET5DL>
2020-08-20 20:37:55 +0300

    demo

---
+++ WebSite3/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <span>Привет, куки!</span>
+            <asp:Button ID="setLangCookieButton" runat="server" Text="Set Language Cookie" OnClick="setLangCookieButton_Click" />
+            <asp:Button ID="deleteLangCookieButton" runat="server" Text="Delete Language Cookie" OnClick="deleteLangCookieButton_Click" />
+        </div>
+        <asp:Label runat="server" ID="output" />
+    </form>
+</body>
+</html>
---
+++ WebSite3/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    public string lang = "ru";
+    protected void Page_Load(object sender, EventArgs e)
+    {   
+        //читаем куки
+        HttpCookie cookie = Request.Cookies["Preferences"];
+        if (cookie != null && cookie["Language"] != null) {
+            output.Text = cookie["Language"];
+        }
+        
+    }
+
+    protected void deleteLangCookieButton_Click(object sender, EventArgs e)
+    {
+        HttpCookie cookie = new HttpCookie("Preferences");
+        cookie.Expires = DateTime.Now.AddDays(-1);
+        Response.Cookies.Add(cookie);
+    }
+
+    protected void setLangCookieButton_Click(object sender, EventArgs e)
+    {
+        // Создать объект cookie-набора.
+        HttpCookie cookie = new HttpCookie("Preferences");
+        // Установить значение в нем.
+        cookie["Language"] = "English";
+        // Добавить в него еще одно значение.
+        cookie["Country"] = "US";
+        // Хранить 1 год
+        cookie.Expires = DateTime.Now.AddYears(1);
+        // Добавить cookie-набор к текущему веб-ответу.
+        Response.Cookies.Add(cookie);
+    }
+}
